bb7cc7d30d0dd28d6c4cc20d330e0aee52c5c921
AKotov-dev <alex_q_2000@mail.ru>
2026-02-05 08:02:25 +0300

    v0.4.1 + HTTP proxy on 127.0.0.1:8889 via gost tunnel

--- ss-cloak-client/backup/start_trd.pas
+++ ss-cloak-client/backup/start_trd.pas
@@ -9,98 +9,103 @@ uses
 
 type
   ShowLogTRD = class(TThread)
-  private
-
-    { Private declarations }
   protected
-  var
     Result: TStringList;
 
     procedure Execute; override;
-
     procedure ShowLog;
     procedure StartTrd;
-
   end;
 
 implementation
 
 uses Unit1;
 
-  { TRD }
-
-
 procedure ShowLogTRD.Execute;
 var
   ExProcess: TProcess;
+  Buffer: array[0..255] of Byte;
+  ReadCnt: LongInt;
+  S, Line: string;
+  P: SizeInt;
 begin
-  try //Старт вывода
+  FreeOnTerminate := True;
+
+  Result := TStringList.Create;
+  ExProcess := TProcess.Create(nil);
+
+  try
     Synchronize(@StartTRD);
-
-    FreeOnTerminate := True; //Уничтожить по завершении
-    Result := TStringList.Create;
-
-    //Рабочий процесс
-    ExProcess := TProcess.Create(nil);
 
     ExProcess.Executable := 'bash';
     ExProcess.Parameters.Add('-c');
-
-    //Если файл лога отсутствует - создаём пустой и читаем до Terminate
     ExProcess.Parameters.Add(
-      '[[ -f ~/.config/ss-cloak-client/ss-cloak-client.log ]] || touch '
-      +
-      '~/.config/ss-cloak-client/ss-cloak-client.log && tail -n 100 -f ~/.config/ss-cloak-client/ss-cloak-client.log 2> >(grep -v truncated >&2)');
+      '[[ -f ~/.config/ss-cloak-client/ss-cloak-client.log ]] || touch ' +
+      '~/.config/ss-cloak-client/ss-cloak-client.log && ' +
+      'tail -n 100 -f ~/.config/ss-cloak-client/ss-cloak-client.log'
+    );
 
     ExProcess.Options := [poUsePipes, poStderrToOutPut];
-    //, poWaitOnExit (синхронный вывод)
-
     ExProcess.Execute;
 
-    //Выводим лог динамически
-    while ExProcess.Running do
-    begin
-      Result.LoadFromStream(ExProcess.Output);
+    S := '';
 
-      //Выводим лог
-      if Result.Count <> 0 then
-        Synchronize(@ShowLog);
+    while not Terminated and ExProcess.Running do
+    begin
+      if ExProcess.Output.NumBytesAvailable > 0 then
+      begin
+        ReadCnt := ExProcess.Output.Read(Buffer, SizeOf(Buffer));
+        if ReadCnt > 0 then
+        begin
+          SetString(Line, PChar(@Buffer[0]), ReadCnt);
+          S := S + Line;
+
+          while True do
+          begin
+            P := Pos(#10, S);
+            if P = 0 then Break;
+
+            Result.Add(Trim(Copy(S, 1, P - 1)));
+            Delete(S, 1, P);
+          end;
+
+          Synchronize(@ShowLog);
+        end;
+      end
+      else
+        Sleep(10);
     end;
 
   finally
-    Result.Free;
     ExProcess.Free;
-    Terminate;
+    Result.Free;
   end;
 end;
 
-{ БЛОК ОТОБРАЖЕНИЯ ЛОГА }
+{ UI }
 
-//Старт вывода
 procedure ShowLogTRD.StartTRD;
 begin
-  //Промотать список вниз (до вывода tail)
   MainForm.LogMemo.SelStart := Length(MainForm.LogMemo.Text);
   MainForm.LogMemo.SelLength := 0;
 end;
 
-//Вывод лога
 procedure ShowLogTRD.ShowLog;
 var
-  i: integer;
+  i: Integer;
 begin
-  //Вывод построчно
   for i := 0 to Result.Count - 1 do
-    MainForm.LogMemo.Lines.Append(Trim(Result[i]));
+    MainForm.LogMemo.Lines.Add(Result[i]);
 
-  //Выводим последние записи минус "n"
-  if MainForm.LogMemo.Lines.Count > 500 then
-    for i := 0 to 500 do
-      MainForm.LogMemo.Lines.Delete(i);
+  Result.Clear;
 
-  //Промотать список вниз
+  // ограничиваем размер лога
+  while MainForm.LogMemo.Lines.Count > 500 do
+    MainForm.LogMemo.Lines.Delete(0);
+
   MainForm.LogMemo.SelStart := Length(MainForm.LogMemo.Text);
   MainForm.LogMemo.SelLength := 0;
 end;
 
 end.
+
